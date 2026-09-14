7b63b50535759e64afc2a89e09b38962a0eb7556
ky4ka_boroxla <dimacmirnov41@gmail.com>
2026-09-14 16:46:06 +0300

    Delete Build2.bat

--- Build2.bat
+++
@@ -1,7 +0,0 @@
-:: вот кому надо кто хочет сделать
-@echo off
-if exist dist rmdir /s /q dist
-python -m PyInstaller --clean --noconsole --onefile --uac-admin --name "hux-huxLauncher" --icon=icon.ico --add-data "hux.gif;." hux-huxLauncher.py
-if exist build rmdir /s /q build
-if exist hux-huxLauncher.spec del /f /q hux-huxLauncher.spec
-pause
